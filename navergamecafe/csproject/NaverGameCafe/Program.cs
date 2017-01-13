d6f594f1ce48c0baebe5eca601ec43b31e36170e
octopus7 <dyunm@naver.com>
2017-01-14 04:03:08 +0900

    c# 프로젝트 (단순 저장만)

---
+++ navergamecafe/csproject/NaverGameCafe/Program.cs
@@ -0,0 +1,48 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaverGameCafe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string html = "";
+            for (int page = 1; page <= 10; page++)
+            {                
+                string url = "http://section.cafe.naver.com/GameCafeList.nhn?search.sortType=rank&search.page=" + page;
+                
+                WebClient wc = new WebClient();
+                wc.Encoding = Encoding.UTF8;
+                html += wc.DownloadString(url);
+            }
+
+            string filepath = DateTime.Now.ToString("yyyyMMddHHmmss") + ".htm";
+            File.WriteAllText(filepath, html);
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            string xPath_li = "/html[*]/body[1]/div[2]/div[2]/div[1]/div[1]/div[3]/div[2]/ul[1]/li[*]";
+            HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes(xPath_li);
+            Console.WriteLine("items : " + nodes.Count);
+
+            foreach (HtmlNode node in nodes)
+            {
+                HtmlNode nodeCafeLink = node.SelectSingleNode("./div[2]/div[1]/h4[1]/a[1]");
+                string link = nodeCafeLink.Attributes["href"].Value;
+                string cafeid = Path.GetFileName(link);
+                string member = node.SelectSingleNode("./div[2]/div[3]/span[2]").InnerHtml;
+                string post = node.SelectSingleNode("./div[2]/div[3]/span[5]").InnerHtml;
+
+                Console.WriteLine(member.PadLeft(10) + post.PadLeft(10) + " " + nodeCafeLink.InnerHtml);
+            }
+        }
+    }
+}
